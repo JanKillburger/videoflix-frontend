7d70dcb6f2ae78f18192b5b86afcde9a1d3e424e
Jan Killburger <jkillburger@gmail.com>
2024-07-27 16:36:35 +0200

    Connect reset password to backend

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { FullBgImageContainerComponent } from '../full-bg-image-container/full-bg-image-container.component';
 import { NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
@@ -11,10 +14,22 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './reset-password.component.scss'
 })
 export class ResetPasswordComponent {
-formData = {password: '', passwordConfirm: ''};
-formErrors = {password: [], passwordConfirm: [], errors: []};
 
-resetPassword() {
-  
-}
+  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+
+  formData = { password: '', passwordConfirm: '' };
+  formErrors = { password: [], passwordConfirm: [], errors: [] };
+
+
+
+  resetPassword() {
+    this.formErrors = { password: [], passwordConfirm: [], errors: [] };
+    this.http.post(`${environment.apiUrl}/reset-password/${this.route.snapshot.paramMap.get('reset-token')}`, { password: this.formData.password }).subscribe({
+      next: () => alert("Password changed successfully. Please login."),
+      error: (err) => {
+        this.formErrors.password = err.error.password;
+        this.formErrors.errors = err.error.general;
+      }
+    })
+  }
 }
