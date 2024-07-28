ffe3011e66ed18a8e16068eb476ed78b23a83815
Jan Killburger <jkillburger@gmail.com>
2024-07-28 21:39:35 +0200

    Add blank screens and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,18 @@ import { VideosComponent } from './videos/videos.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { WatchComponent } from './watch/watch.component';
+import { VideoDetailComponent } from './video-detail/video-detail.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'login/:activation-token', component: LoginComponent},
     {path: 'signup', component: SignupComponent},
-    {path: 'videos', component: VideosComponent},
+    {path: 'browse', component: VideosComponent},
     {path: 'forgot-password', component: ForgotPasswordComponent},
     {path: 'reset-password/:reset-token', component: ResetPasswordComponent},
     {path: 'imprint', component: ImprintComponent},
+    {path: 'watch/:id', component: WatchComponent},
+    {path: 'details/:id', component: VideoDetailComponent},
 ];
