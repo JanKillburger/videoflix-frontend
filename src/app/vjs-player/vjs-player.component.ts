ce9e9142c63509eb786dc43105c88ce90f86a69f
Jan Killburger <jkillburger@gmail.com>
2024-08-01 23:45:08 +0200

    Customize vjs controls layout

--- src/app/vjs-player/vjs-player.component.ts
+++ src/app/vjs-player/vjs-player.component.ts
@@ -1,7 +1,25 @@
-import {Component, ElementRef, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import videojs from 'video.js';
 import Player from 'video.js/dist/types/player';
 import { AuthService } from '../auth.service';
+import { textContent } from 'video.js/dist/types/utils/dom';
+
+const VjsComponent = videojs.getComponent('Component');
+
+class ControlBarSection extends VjsComponent {
+  constructor(player: Player, options: any = {}) {
+    super(player, options);
+  }
+
+  override createEl() {
+    return videojs.dom.createEl('div', {
+      className: 'vjs-control-bar-section'
+    })
+  }
+}
+
+videojs.registerComponent('ControlBarSection', ControlBarSection);
+
 
 @Component({
   selector: 'app-vjs-player',
@@ -16,34 +34,52 @@ import { AuthService } from '../auth.service';
 })
 
 export class VjsPlayerComponent implements OnInit, OnDestroy {
-  @ViewChild('target', {static: true}) target!: ElementRef;
-
-  // See options: https://videojs.com/guides/options
-  @Input({required: true}) options!: {
-      fluid: boolean,
-      autoplay: boolean | 'muted' | 'play' | 'any',
-      sources: {
-        src: string,
-        type: string
-      }[]      
-  };
-
+  @ViewChild('target', { static: true }) target!: ElementRef;
+  @Input() videoTitle = 'Majestic record player';
   player!: Player;
 
   constructor(
     private elementRef: ElementRef,
     private auth: AuthService
   ) {
-    
+
   }
 
   // Instantiate a Video.js player OnInit
   ngOnInit() {
-    this.player = videojs(this.target.nativeElement, this.options, function onPlayerReady() {
+    this.player = videojs(this.target.nativeElement, {
+      sources: [{ src: 'http://127.0.0.1/media/videos/1_master.m3u8', type: 'application/x-mpegURL' }],
+      fluid: true,
+      autoplay: 'muted',
+      controlBar: {
+        
+        skipButtons: {
+          backward: 10,
+          forward: 10
+        },
+        volumePanel: {},
+        
+        button: {
+          controlText: this.videoTitle,
+          className: 'vjs-visible-text vjs-custom-title'
+        },
+        subtitlesButton: {},
+        fullscreenControl: {},
+        progressControl: false,
+        pictureInPictureToggle: false,
+        currentTimeDisplay: false,
+        remainingTimeDisplay: false,
+        playToggle: {}
+      },
+      playbackRates: [1, 1.5, 2],
+      controlBarSection: {}
+    }, function onPlayerReady() {
       
     });
+    this.player.getChild('ControlBarSection')?.addChild('currentTimeDisplay');
+    this.player.getChild('ControlBarSection')?.addChild('progressControl');
+    this.player.getChild('ControlBarSection')?.addChild('remainingTimeDisplay');
   }
-
   // Dispose the player OnDestroy
   ngOnDestroy() {
     if (this.player) {
@@ -51,3 +87,5 @@ export class VjsPlayerComponent implements OnInit, OnDestroy {
     }
   }
 }
+
+/*[options]="{controlBar: {skipButtons: {backward: }}, fluid: true, autoplay: 'muted', sources: [{src: video.src, type: 'application/x-mpegURL'}]}"*/
